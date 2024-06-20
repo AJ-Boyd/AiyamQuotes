2db96a88ba70454c6952ef3e8f3e2b02ee5bff78
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-06-20 02:22:17 -0400

    current working version

--- generator/generate_quote.py
+++ generator/generate_quote.py
@@ -7,6 +7,37 @@ desc: generates a quote that embodies a list of adjectives
 
 from langchain_community.llms import Ollama
 from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_core.prompts import PromptTemplate
+import re
 
+SYS_PROMPT = "You are Aiyam, an AI language tool committed to providing short, stupid, nonsensical quotes to entertain. The quotes should be bad enough that it's clear an AI made them."
+PROMPT = """
+Put your generated quote in angle brackets. Generate one single unique, random, and entertaining quote in less than 15 words that subtly embodies different themes. 
+It's important that the quotes do not directly name the theme they embody. 
+
+Here is the list of themes: 
+"""
 def gen_quote(adjectives: list) -> str:
-    return "I Love AI!!!!!"
+    llm = Ollama(
+        model = "mistral",
+        verbose = False,
+        # callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]),
+        temperature = 1.82,
+        system = SYS_PROMPT,
+        top_k = 50,
+        top_p = 0.95
+    )
+    response = llm.invoke(PROMPT + str(adjectives))
+    print("response:", response)
+    # text cleaning!
+    response = response.strip()
+    pattern = r'<([^\"]*)>'
+    match = re.search(pattern, response)
+    if match:
+        response = match.group(1)
+    if response.startswith("\"") == False:
+        response = "\"" + response
+    if response.endswith("\"") == False:
+        response += "\""
+
+    return response
--- generator/routes.py
+++ generator/routes.py
@@ -1,13 +1,27 @@
-# stores roots for the website
-from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for
+"""
+auth: AJ Boyd
+date: 6/19/2024
+file: routes.py
+desc: handles the form requests for the generator
+"""
+
+from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for, session
+from .generate_quote import gen_quote
 
 routes = Blueprint("routes", __name__)
 
-@routes.route('/', methods=['GET', 'POST'])
+@routes.route('/', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
-        print("post request received!")
-        selected_adjectives = request.form.getlist("adjective")
-        print("Selected Adjectives:", selected_adjectives)
-        return redirect("/")
-    return render_template("index.html")
+    # quote = request.args.get('quote', 'Dummy quote')
+    quote = session.get('quote', 'Dummy quote')
+    return render_template("index.html", quote=quote)
+
+@routes.route('/submit', methods=["POST"])
+def submit():
+    print("post request received!")
+    adjectives = request.form.getlist("adjective")
+    print("Selected Adjectives:", adjectives)
+    quote = gen_quote(adjectives)
+    print(quote)
+    session['quote'] = quote
+    return redirect(url_for('routes.home', _anchor='middle'))
